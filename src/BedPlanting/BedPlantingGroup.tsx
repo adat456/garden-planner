5d659c08bd99761ff9d9ea95b1c0602a841f4327
Ada Truong <adat456@gmail.com>
2023-06-23 21:24:03 -0400

    Add fixed position, abbreviated seed basket so that user can preview seed selection while browsing search results

--- src/BedPlanting/BedPlantingGroup.tsx
+++ src/BedPlanting/BedPlantingGroup.tsx
@@ -8,16 +8,26 @@ import PlantSearch from './PlantSearch';
 const BedPlantingGroup: React.FC = function() {
     const [ plantPicks, setPlantPicks ] = useState<plantPickDataInterface[]>([]);
     const [ curPlantPick, setCurPlantPick ] = useState<plantPickDataInterface | null>(null);
+    const [ abbrPlantPicksVis, setAbbrPlantPicksVis ] = useState(false);
 
     const { bedId } = useParams();
 
     function generatePlantPicks() {
         const plantPicksArr = plantPicks.map(plant => {
             return (
-                <PlantPick key={`plant-pick-${plant.id}`} plant={plant} plantPicks={plantPicks} setPlantPicks={setPlantPicks} setCurPlantPick={setCurPlantPick} />
+                <PlantPick key={`plant-pick-${plant.id}`} plant={plant} plantPicks={plantPicks} setPlantPicks={setPlantPicks} setCurPlantPick={setCurPlantPick} abbreviated={false} />
             );
         });
         return plantPicksArr;
+    };
+
+    function generateAbbrPlantPicks() {
+        const abbrPlantPicksArr = plantPicks.map(plant => {
+            return (
+                <PlantPick key={`plant-pick-${plant.id}`} plant={plant} plantPicks={plantPicks} setPlantPicks={setPlantPicks} setCurPlantPick={setCurPlantPick} abbreviated={true} />
+            );
+        });
+        return abbrPlantPicksArr;
     };
 
     useEffect(() => {
@@ -48,11 +58,25 @@ const BedPlantingGroup: React.FC = function() {
         <div className="bed-planting-group">
             <BedPlantingGrid curPlantPick={curPlantPick} />
             <section className="seed-basket">
-                <h2>Seed Basket</h2>
+                <h2>SEED BASKET</h2>
                 <hr />
                 <ul>
                     {generatePlantPicks()}
                 </ul>
+            </section>
+            <section className="abbr-seed-basket">
+                {abbrPlantPicksVis ?
+                    <ul>
+                        {generateAbbrPlantPicks()}
+                    </ul> : null
+                }
+                <div onClick={() => setAbbrPlantPicksVis(!abbrPlantPicksVis)}>
+                    <h2>{`SEED BASKET (${plantPicks.length})`}</h2>
+                    {abbrPlantPicksVis ?
+                        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.72798 15.795L3.72798 7.795C3.10356 6.79593 3.82183 5.5 4.99998 5.5L15 5.5C16.1781 5.5 16.8964 6.79593 16.272 7.795L11.272 15.795C10.6845 16.735 9.31549 16.735 8.72798 15.795Z" /></svg> :
+                        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11.272 5.205L16.272 13.205C16.8964 14.2041 16.1782 15.5 15 15.5H5.00002C3.82186 15.5 3.1036 14.2041 3.72802 13.205L8.72802 5.205C9.31552 4.265 10.6845 4.265 11.272 5.205Z" /></svg>
+                    }
+                </div>
             </section>
             <PlantSearch plantPicks={plantPicks} setPlantPicks={setPlantPicks} />
         </div>
